abf78d51c49fabcfe65041e23a242ab61241eae3
Anil Madhavapeddy <anil@recoil.org>
2013-12-20 15:57:24 +0000

    Remove all the Markdown variants except `Omd`, which now claims the `Cow.Markdown` module name.
    Bump version to 0.9.0

--- lib/markdown.mli
+++ lib/markdown.mli
@@ -1,69 +1,21 @@
 (*
-  Copyright (c) 2009 Mauricio Fernández <mfp@acm.org>
-  Copyright (c) 2009-2010 Anil Madhavapeddy <anil@recoil.org>
-  Copyright (c) 2010 Thomas Gazagnaire <thomas@gazagnaire.org>
+ * Copyright (c) 2013 Richard Mortier <mort@cantab.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
 
-  Permission is hereby granted, free of charge, to any person
-  obtaining a copy of this software and associated documentation
-  files (the "Software"), to deal in the Software without
-  restriction, including without limitation the rights to use,
-  copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the
-  Software is furnished to do so, subject to the following
-  conditions:
+type t = Html.t
 
-  The above copyright notice and this permission notice shall be
-  included in all copies or substantial portions of the Software.
+val to_string: t -> string
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-  OTHER DEALINGS IN THE SOFTWARE.
-*)
-
-type ref = { src : string; desc : string }
-
-type paragraph =
-    Normal of par_text
-  | Html of Html.t
-  | Pre of string * string option
-  | Heading of int * par_text
-  | Quote of paragraph list
-  | Ulist of paragraph list * paragraph list list
-  | Olist of paragraph list * paragraph list list
-
-and par_text = text list
-
-and text =
-    Text of string
-  | Emph of string
-  | Bold of string
-  | Struck of par_text
-  | Code of string
-  | Link of href
-  | Anchor of string
-  | Image of img_ref
-
-and href = { href_target : string; href_desc : string; }
-
-and img_ref = { img_src : string; img_alt : string; }
-
-and t = paragraph list
-
-val to_html : t -> Html.t
-
-val of_string : string -> t
-
-(** Create a heading *)
-val id_of_heading: paragraph -> string
-
-(** Extract a HTML table of contents from markdown elements. Depth can
-    be modified with the corresponding optional argument. *)
-val to_html_toc:
-  ?wrap_list:(depth:int -> Html.t -> Html.t) ->
-  ?wrap_item:(depth:int -> heading:paragraph -> Html.t -> Html.t) ->
-  ?depth:int -> t -> Html.t
+val of_string: string -> t
