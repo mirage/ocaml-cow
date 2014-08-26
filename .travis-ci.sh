d432edcd0e35a473a4f6ddfd8cbbbc49b5142801
Anil Madhavapeddy <anil@recoil.org>
2014-08-26 15:03:31 +0100

    Travis: add OCaml 4.02.0 and OPAM 1.2.0 into the test matrix

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,11 +2,12 @@ OPAM_DEPENDS="re ulex uri xmlm ezjsonm dyntype ounit omd"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
+3.12.1,1.1.*) ppa=avsm/ocaml312+opam11 ;;
 4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.00.1,1.1.*) ppa=avsm/ocaml40+opam11 ;;
 4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.01.0,1.1.*) ppa=avsm/ocaml41+opam11 ;;
+4.02.0,1.2.*) ppa=avsm/ppa-opam-experimental ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
