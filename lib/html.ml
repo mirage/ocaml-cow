mirage/ocaml-cow

commit 6b08a0857d12db7cad214094e416a783c0485338

fixed a bug in the Html.Create library in which I was trying to use a  antiquotation in an HTML quotation.

lib/html.ml
@@ -227,7 +227,8 @@ module Create = struct
     in <:html< <ol>$els$</ol> >>
 
   let stylesheet css =
-    <:html< <style type="text/css">$css:css$</style> >>
+    let css = Cow.Css.to_string css in
+    <:html< <style type="text/css">$str:css$</style> >>
 
   let table ?(flags = [Headings_fst_row]) ~row tbl =
     let h_fst_col = ref false in

commit d76852f0b7b9b6e4b1bbd12f12ae9a7a86a602fd

fixed a problem introduced in commit 6b08a08 which kept the library from compiling

lib/html.ml
@@ -245,7 +245,7 @@ module Create = struct
       | Sideways -> side := true;
       ();)
       flags in
-    fun ~row tbl =
+    let aux ~row tbl =
       let rows = List.map row tbl in
       let rows =
         if !side then
@@ -287,5 +287,6 @@ module Create = struct
       let rows = List.map (fun r -> let r = List.flatten r in <:html<<tr>$r$</tr>&>>) rows in
       let rows = concat rows in
       <:html<<table>$rows$</table>&>>
+    in aux
 
 end